f4c6de1168d0ffa3a8baf20d5d58f79b35e764bb
Igor Araujo <igor@Igor>
2022-10-26 17:29:23 -0300

    fix(mailconfig): change mailconfig

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -1,7 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import { injectable, inject } from 'tsyringe';
 import { SES } from 'aws-sdk';
-
+import mailConfig from '@config/mail';
 import { ISendMailDTO } from '@shared/container/providers/MailProvider/dtos/ISendMailDTO';
 import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
 
@@ -29,10 +29,11 @@ export class SESMailProvider implements IMailProvider {
         subject,
         templateData,
     }: ISendMailDTO): Promise<void> {
+        const { name, email } = mailConfig.defaults.from;
         await this.client.sendMail({
             from: {
-                name: from.name,
-                address: from.email,
+                name: from?.name || name,
+                address: from?.email || email,
             },
             to: {
                 name: to.name,
