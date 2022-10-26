d3c4df18e456386501452479937d642aec1cc0d8
Igor Araujo <igor@Igor>
2022-10-26 16:16:36 -0300

    fix(ses service): change service email

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -6,7 +6,6 @@ import { ISendMailDTO } from '@shared/container/providers/MailProvider/dtos/ISen
 import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
 
 import { IMailTemplateProvider } from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
-import mailConfig from '@config/mail';
 
 @injectable()
 export class SESMailProvider implements IMailProvider {
@@ -30,12 +29,10 @@ export class SESMailProvider implements IMailProvider {
         subject,
         templateData,
     }: ISendMailDTO): Promise<void> {
-        const { name, email } = mailConfig.defaults.from;
-
         await this.client.sendMail({
             from: {
-                name: from.name || name,
-                address: from.email || email,
+                name: from.name,
+                address: from.email,
             },
             to: {
                 name: to.name,
