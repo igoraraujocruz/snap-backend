237935602e4b988b2b705169448aecc8c7b4733d
Igor <igoraraujocruzz@gmail.com>
2023-06-18 00:30:33 -0300

    fix(return port): return

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -74,5 +74,5 @@ app.use((error: Error, _: Request, response: Response, __: NextFunction) => {
 });
 
 app.listen(process.env.APP_PORT, () => {
-    console.log('Server started');
+    console.log(`Server started on port ${process.env.APP_PORT}`);
 });
